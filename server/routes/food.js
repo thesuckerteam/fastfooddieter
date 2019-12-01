4ca6d3e3618b312cd0c3fe759b59f1ff3b298c20
bankkeez <sethitikul@gmail.com>
2019-12-02 03:18:49 +0700

    add more endpoints

--- server/routes/food.js
+++ server/routes/food.js
@@ -8,6 +8,26 @@ router.get('/test-food', async (req, res) => {
     res.send(await tmp.find({}).toArray());
 });
 
+router.get('/foods/salads', async (req, res) => {
+  const collection = await loadFoods("salad");
+  res.send(await collection.find({}).toArray());
+});
+
+router.get('/foods/chickens', async (req, res) => {
+  const collection = await loadFoods("chicken");
+  res.send(await collection.find({}).toArray());
+});
+
+router.get('/foods/rice', async (req, res) => {
+  const collection = await loadFoods("rice");
+  res.send(await collection.find({}).toArray());
+});
+
+router.get('/foods/burgers', async (req, res) => {
+  const collection = await loadFoods("burger");
+  res.send(await collection.find({}).toArray());
+});
+
 async function loadFoods(collection) {
     const uri = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + process.env.DB_URL;
     const client = await MongoClient.connect(uri,{useNewUrlParser: true}
