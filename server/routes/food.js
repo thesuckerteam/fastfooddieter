673ff761f9a46620196c6a0835a0ca4d99903a13
bankkeez <sethitikul@gmail.com>
2019-12-02 10:41:07 +0700

    edit something

--- server/routes/food.js
+++ server/routes/food.js
@@ -8,24 +8,44 @@ router.get('/test-food', async (req, res) => {
     res.send(await tmp.find({}).toArray());
 });
 
-router.get('/foods/salads', async (req, res) => {
+router.get('/salads', async (req, res) => {
   const collection = await loadFoods("salad");
   res.send(await collection.find({}).toArray());
 });
 
-router.get('/foods/chickens', async (req, res) => {
+router.get('/chickens', async (req, res) => {
   const collection = await loadFoods("chicken");
   res.send(await collection.find({}).toArray());
 });
 
-router.get('/foods/rice', async (req, res) => {
+router.get('/rice', async (req, res) => {
   const collection = await loadFoods("rice");
   res.send(await collection.find({}).toArray());
 });
 
-router.get('/foods/burgers', async (req, res) => {
+router.get('/burgers', async (req, res) => {
   const collection = await loadFoods("burger");
   res.send(await collection.find({}).toArray());
+});
+
+router.get('/salads/limit-calories/:calories', async (req, res) => {
+  const collection = await loadFoods("salad");
+  res.send(await collection.find({calories : { $lte: req.params.calories }}).toArray());
+});
+
+router.get('/chickens/limit-calories/:calories', async (req, res) => {
+  const collection = await loadFoods("chicken");
+  res.send(await collection.find({calories : { $lte: req.params.calories }}).toArray());
+});
+
+router.get('/rice/limit-calories/:calories', async (req, res) => {
+  const collection = await loadFoods("rice");
+  res.send(await collection.find({calories : { $lte: req.params.calories }}).toArray());
+});
+
+router.get('/burgers/limit-calories/:calories', async (req, res) => {
+  const collection = await loadFoods("burger");
+  res.send(await collection.find({calories : { $lte: req.params.calories }}).toArray());
 });
 
 async function loadFoods(collection) {
@@ -34,11 +54,5 @@ async function loadFoods(collection) {
   );
   return client.db(process.env.DB_NAME).collection(collection);
 }
-
-
-
-
-
-
 
 module.exports = router;
